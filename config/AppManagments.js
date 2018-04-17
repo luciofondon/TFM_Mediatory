f3527ead1dd8bfb58d32c164585aaa9f2cb7a57d
Lucio Fondón <luciofondonterron@gmail.com>
2018-04-17 09:49:15 +0200

    Create AppManagments.js

---
+++ config/AppManagments.js
@@ -0,0 +1,37 @@
+
+
+apps: [
+    {
+        id: "jira",
+        name: "Jira Atlassian",
+        logo: "https://www.redmineup.com/cms/assets/thumbnail/33796/600/jira_logo.png",
+        authentication : {
+            url: "api/",
+            type: "body",
+            params: [
+                {id: "email", name : "Correo electrónico", description: "Email del usuario con permisos de administrador"},
+                {id: "password", name: "Contraseña", description: "Contraseña del usuario con permisos de administrador"}
+            ]
+        },
+        api: [
+            addProject: "/rest/api/2/project",
+            getProject: "/rest/api/2/project"
+        ]
+    },
+    {
+        id: "redmine",
+        name: "Redmine",
+        logo: "https://blog.desdelinux.net/wp-content/uploads/2013/05/redmine.png",
+        authentication : {
+            url: "api/",
+            type: "header"
+            params: [
+                {id: "token", name : "Token", description: "Token disponible a través de la cuenta de administrador"}
+            ]
+        },
+        api: [
+            addProject: "/redmine/projects.json",
+            getProject: "/redmine/projects.json"
+        ]
+    }
+]
