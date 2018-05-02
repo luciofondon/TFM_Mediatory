4f9ad3fc82a008a56af240130d6286a1a3fff676
Lucio Fondón <luciofondonterron@gmail.com>
2018-05-02 18:11:52 +0200

    Create TaskManagerDAO.js

---
+++ src/DAOS/TaskManagerDAO.js
@@ -0,0 +1,19 @@
+var  httpService = require('../services/HttpService');
+
+
+exports.checkComunication = function(req, res) {
+    checkComunication(req, res);
+};
+
+exports.getApps = function(req, res) {
+    getApps(req, res);
+};
+
+function getApps(req, res){
+
+}
+
+
+function getApps(req, res){
+
+}
