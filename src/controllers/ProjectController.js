51d29e7adc9d1291b8b96ef999f0fb88095939ad
Lucio Fondón <luciofondonterron@gmail.com>
2018-05-02 18:16:04 +0200

    Update ProjectController.js

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -8,8 +8,12 @@ var  projectService = require('../services/ProjectService');
 module.exports = function(db) {
 
     return {
-        create: function(req, res) {
+        createProject: function(req, res) {
             projectService.createProject(req, res);
+        },
+		
+		readAllProject: function(req, res) {
+            projectService.readAllProject(req, res);
         }
     }
 }
